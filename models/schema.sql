4470cb4ff9eeaddafb14bacde1e11b0b5c371f4d
codereber <jamesmpyle@gmail.com>
2019-05-02 19:54:45 -0400

    new update

--- models/schema.sql
+++ models/schema.sql
@@ -1,32 +1,33 @@
+DROP DATABASE IF EXISTS lastmingig_db;
 CREATE DATABASE lastmingig_db;
 USE lastmingig_db;
 
-CREATE TABLE employee
-(
-	id int NOT NULL AUTO_INCREMENT,
-	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	name varchar(255) NOT NULL,
-    location varchar(255) NOT NULL,
-    craft varchar(255) NOT NULL,
-    sex varchar(255),
-    `img` LONGBLOB,
-    description varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-	available BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE employee
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- 	name varchar(255) NOT NULL,
+--     location varchar(255) NOT NULL,
+--     craft varchar(255) NOT NULL,
+--     sex varchar(255),
+--     `img` LONGBLOB,
+--     description varchar(255) NOT NULL,
+--     email varchar(255) NOT NULL,
+-- 	available varchar(255) NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
 
-CREATE TABLE employer
-(
-	id int NOT NULL auto_increment,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    name varchar(255) NOT NULL,
-    location varchar(255) NOT NULL,
-    `img` LONGBLOB,
-    sex varchar(255),
-    description varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    available BOOLEAN Default false,
-    primary key (id)
+-- CREATE TABLE employer
+-- (
+-- 	id int NOT NULL auto_increment,
+--     `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     name varchar(255) NOT NULL,
+--     location varchar(255) NOT NULL,
+--     `img` LONGBLOB,
+--     sex varchar(255),
+--     description varchar(255) NOT NULL,
+--     email varchar(255) NOT NULL,
+--     available varchar(255) NOT NULL,
+--     primary key (id)
        
-);
+-- );
