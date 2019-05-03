35405c822839c5124a9fc8591f52cbfc2af3faa7
codereber <jamesmpyle@gmail.com>
2019-05-02 21:39:56 -0400

    api_routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,23 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/listing", function(req, res) {
+    db.listing.findAll({}).then(function(dblisting) {
+      res.json(dblisting);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/listing", function(req, res) {
+    db.listing.create(req.body).then(function(dblisting) {
+      res.json(dblisting);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/listing/:id", function(req, res) {
+    db.listing.destroy({ where: { id: req.params.id } }).then(function(dblisting) {
+      res.json(dblisting);
     });
   });
 };
